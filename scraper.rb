486e4ffb4e1f12f2ca20525999aa3b5ad523fbb6
Tony Bowden <tony@tmtm.com>
2017-05-30 19:14:08 +0100

    Also fetch via P39

--- scraper.rb
+++ scraper.rb
@@ -3,10 +3,13 @@
 
 require 'wikidata/fetcher'
 
+# Find all P39s
+query = 'SELECT DISTINCT ?item WHERE { ?item wdt:P39 wd:Q28479658 }'
+p39s = EveryPolitician::Wikidata.sparql(query)
+
 en = WikiData::Category.new( 'Category:Members of the Estates of Curaçao', 'en').member_titles |
      WikiData::Category.new( 'Category:Presidents of the Estates of Curaçao', 'en').member_titles
 nl = WikiData::Category.new( 'Categorie:Curaçaos politicus', 'nl').member_titles
 
-
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en, nl: nl })
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: en, nl: nl })
 
